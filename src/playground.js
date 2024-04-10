0ba8fa501b98bbdb61d3d4645c205e1ae02d9416
embryobirthday <alexinverda@gmail.com>
2024-04-10 10:58:41 -0400

    questions done

--- src/playground.js
+++ src/playground.js
@@ -10,3 +10,13 @@
 //   getAllYCoordinates,
 // } = require('./from-scratch');
 // const { uppercaseAll, destructureCoordinates } = require('./modify');
+
+
+
+const favoriteLocations = ["New York City", "Jersey City"];
+const addBostonToFavorites = (locations) => {
+  locations.unshift("Boston");
+};
+console.log(favoriteLocations)
+addBostonToFavorites(favoriteLocations);
+console.log(favoriteLocations);
